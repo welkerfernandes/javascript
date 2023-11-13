b428adc9ecc7bf06f9467932b02337b82ea1d353
welkerfernandes <welkerfc@icloud.com>
2023-11-13 10:32:10 -0300

    numbers

---
+++ number.js
@@ -0,0 +1,13 @@
+const number = 5;
+
+console.log(number)
+
+//soma
+const number_1 = 15;
+
+console.log(number_1 + 28);
+
+//converter numero para string
+
+console.log(number_1.toString())
+console.log(typeof number_1)
