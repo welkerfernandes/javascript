89ef91fb9f5d09a905f613dd15cccc584a757ad2
welkerfernandes <welkerfc@icloud.com>
2023-11-13 10:18:22 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,16 @@
+//var é a forma antiga então pode esquecer ela não tem block scope
+//var message = 'Olá mundo'
+// vamos usar const e let
+
+// com const não consigo alterar depois
+//esse exemplo mostra erro no console
+//const teste = 'Olá mundo com Const'
+//teste = 'tudo bem brasil?'
+//console.log(teste)
+
+
+// consigo alterar depois
+let message = 'Oi Let'
+message = 'Oi let é o caramba'
+
+console.log(message)
