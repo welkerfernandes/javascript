925108298dda881f3c0b06dc084b06e1674726b3
welkerfernandes <welkerfc@icloud.com>
2023-11-14 00:56:33 -0300

    condicionais

---
+++ condicionais.js
@@ -0,0 +1,30 @@
+const index = 201;
+const sanu = 50;
+
+if (index === 20){
+	//console.log('index é = a 20')
+}else if(index == 201){
+	//console.log('index é igual a 201');
+}else{
+	//console.log('index é diferente')
+}
+
+//&& = and   || = OR
+
+if (index == 201 && sanu == 50){
+	//console.log('ok')
+}
+
+//switch case
+
+const car = 'mercedes'
+
+switch(car){
+	case 'mercedes':
+	console.log('é uma mercedes')
+	break;
+
+	case 'Evoque':
+	console.log('é uma evoque')
+	break;
+}
